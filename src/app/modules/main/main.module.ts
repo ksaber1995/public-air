a3cba6a0ade06a8855a1f24fcff629a0ced5e9e4
Karim Ali Saber <KSaber@octiga.com>
2024-02-02 18:29:45 +0200

    adding tablet

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './components/home/home.component';
 import { SeverityBarComponent } from './components/severity-bar/severity-bar.component';
 import { StationMarkerComponent } from './components/station-marker/station-marker.component';
 import { SharedModule } from '../shared/shared.module';
+import { TabComponent } from './components/tab/tab.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     SeverityBarComponent,
-    StationMarkerComponent
+    StationMarkerComponent,
+    TabComponent
   ],
 
   imports: [
