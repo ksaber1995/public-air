0e03fcf02a4b57aeca9f5f5001acb1a4ae01dcc9
Karim Ali <karim@etree.com.sa>
2024-03-05 16:00:40 +0200

    adding lang toggle

--- src/app/modules/main/components/container/container.component.ts
+++ src/app/modules/main/components/container/container.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { LocalizationService } from '../../../shared/services/localization.service';
+import { delay } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-container',
@@ -7,17 +9,48 @@ import { LocalizationService } from '../../../shared/services/localization.servi
   styleUrl: './container.component.scss'
 })
 export class ContainerComponent implements OnInit {
-  isLoading: boolean = false;
+  isLoading: boolean = true;
   constructor(
-    private localization: LocalizationService
+    private localization: LocalizationService,
+    private renderer: Renderer2
 
   ) { }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.isLoading = true
-    }, 1000);
-    this.setLangs()
+
+    this.localization.getCurrentLanguage().pipe(delay(1000))
+      .subscribe(res => {
+        this.loadGoogleMapsScript(res)
+      })
+
+  }
+
+
+  loadGoogleMapsScript(language: string) {
+    this.isLoading = true
+    const existingScript = document.getElementById('google-maps-script');
+    if (existingScript) {
+      this.renderer.removeChild(document.body, existingScript);
+    }
+
+    const script = this.renderer.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDDQ3S08D_41Ll3a2GjTE28KGQR-G6XvmM&libraries=places&language=${language}`;
+    script.id = 'google-maps-script';
+    script.async = true;
+    script.defer = true;
+
+    script.onload = () => {
+      // Google Maps API loaded successfully
+      // You can initialize your map here if needed
+      this.isLoading = false;
+    };
+
+    script.onerror = () => {
+      console.error('Error loading Google Maps API script.');
+    };
+
+    this.renderer.appendChild(document.body, script);
+
   }
 
   setLangs() {
